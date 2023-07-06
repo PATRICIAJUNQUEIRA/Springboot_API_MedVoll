a735f86f070d417f9defda3080faae470bf4c316
Patrícia Junqueira <96187596+PATRICIAJUNQUEIRA@users.noreply.github.com>
2023-07-05 21:19:21 -0300

    Interfaces Repositoty

--- Spring-boot-API-MedVoll/src/main/java/med/voll/api/controller/MedicoController.java
+++ Spring-boot-API-MedVoll/src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,20 +1,26 @@
 package med.voll.api.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import med.voll.api.medico.DadosCadastroMedico;
+import med.voll.api.medico.Medico;
+import med.voll.api.medico.MedicoRepository;
 
 // Carrega  e faz as atualizações do projeto
 @RestController
 @RequestMapping("medicos") 
 public class MedicoController {
 	
+	@Autowired
+	private MedicoRepository repository;
+	
 	@PostMapping
 	public void cadastrar(@RequestBody DadosCadastroMedico dados) {
-		System.out.println(dados);
+		repository.save(new Medico(dados));
 	}
 }
 
---
+++ Spring-boot-API-MedVoll/src/main/java/med/voll/api/endereco/Endereco.java
@@ -0,0 +1,74 @@
+package med.voll.api.endereco;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Endereco {
+
+        private String logradouro;
+        private String bairro;
+        private String cep;
+        private String numero;
+        private String complemento;
+        private String cidade;
+        private String uf;
+        
+        public Endereco(DadosEndereco dados) {
+            this.logradouro = dados.logradouro();
+            this.bairro = dados.bairro();
+            this.cep = dados.cep();
+            this.uf = dados.uf();
+            this.cidade = dados.cidade();
+            this.numero = dados.numero();
+            this.complemento = dados.complemento();
+        }
+
+		public String getLogradouro() {
+			return logradouro;
+		}
+		public void setLogradouro(String logradouro) {
+			this.logradouro = logradouro;
+		}
+		public String getBairro() {
+			return bairro;
+		}
+		public void setBairro(String bairro) {
+			this.bairro = bairro;
+		}
+		public String getCep() {
+			return cep;
+		}
+		public void setCep(String cep) {
+			this.cep = cep;
+		}
+		public String getNumero() {
+			return numero;
+		}
+		public void setNumero(String numero) {
+			this.numero = numero;
+		}
+		public String getComplemento() {
+			return complemento;
+		}
+		public void setComplemento(String complemento) {
+			this.complemento = complemento;
+		}
+		public String getCidade() {
+			return cidade;
+		}
+		public void setCidade(String cidade) {
+			this.cidade = cidade;
+		}
+		public String getUf() {
+			return uf;
+		}
+		public void setUf(String uf) {
+			this.uf = uf;
+		}
+}
