f666454ebdbcc078f6edd5ec8fd0d7cf23fe3cf3
Patrícia Junqueira <96187596+PATRICIAJUNQUEIRA@users.noreply.github.com>
2023-07-10 20:31:06 -0300

    Ordenação e Formatação SQL

--- Spring-boot-API-MedVoll/src/main/java/med/voll/api/controller/MedicoController.java
+++ Spring-boot-API-MedVoll/src/main/java/med/voll/api/controller/MedicoController.java
@@ -3,6 +3,7 @@ package med.voll.api.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -29,7 +30,7 @@ public class MedicoController {
         repository.save(new Medico(dados));
     }
     @GetMapping
-    public Page<DadosListagemMedico> listar(Pageable paginacao) {
+    public Page<DadosListagemMedico> listar(@PageableDefault(size = 10, sort = "nome")Pageable paginacao) {
         return repository.findAll(paginacao).map(DadosListagemMedico::new);
     }
 }
