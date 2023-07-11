699f65591445839eeea72f28f455a1a95c1dbfcf
Patrícia Junqueira <96187596+PATRICIAJUNQUEIRA@users.noreply.github.com>
2023-07-10 21:43:04 -0300

    PUT CRUD Atualizações

--- Spring-boot-API-MedVoll/src/main/java/med/voll/api/controller/MedicoController.java
+++ Spring-boot-API-MedVoll/src/main/java/med/voll/api/controller/MedicoController.java
@@ -6,12 +6,14 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
+import med.voll.api.medico.DadosAtualizacaoMedico;
 import med.voll.api.medico.DadosCadastroMedico;
 import med.voll.api.medico.DadosListagemMedico;
 import med.voll.api.medico.Medico;
@@ -32,5 +34,13 @@ public class MedicoController {
     @GetMapping
     public Page<DadosListagemMedico> listar(@PageableDefault(size = 10, sort = "nome")Pageable paginacao) {
         return repository.findAll(paginacao).map(DadosListagemMedico::new);
+    }
+    
+    @PutMapping
+    @Transactional
+    public void atualizar(@RequestBody @Valid DadosAtualizacaoMedico dados) {
+    	var medico = repository.getReferenceById(dados.id());
+    	medico.atualizarInformacoes(dados);
+    	
     }
 }
--- Spring-boot-API-MedVoll/src/main/java/med/voll/api/medico/Medico.java
+++ Spring-boot-API-MedVoll/src/main/java/med/voll/api/medico/Medico.java
@@ -8,6 +8,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -46,7 +47,7 @@ public class Medico{
                     this.especialidade = dados.especialidade();
             this.endereco = new Endereco(dados.endereco());
         }
- 
+        
 		public String getNome() {
 			return nome;
 		}
@@ -81,5 +82,23 @@ public class Medico{
 		
 		public Especialidade getEspecialidade() {
 			return especialidade;
+		}
+
+		public Long getId() {
+			return id;
+		}
+
+		public void atualizarInformacoes(@Valid DadosAtualizacaoMedico dados) {
+			if(dados.nome() != null) {
+				this.nome = dados.nome();
+			}
+			
+			if (dados.telefone() != null) {
+				this.telefone = dados.telefone();
+			}
+			
+			if (dados.endereco() != null) {
+	            this.endereco.atualizarInformacoes(dados.endereco());
+	        }
 		}
 }
