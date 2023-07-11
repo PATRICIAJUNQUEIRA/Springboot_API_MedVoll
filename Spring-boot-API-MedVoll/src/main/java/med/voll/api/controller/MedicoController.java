b24ba78ebe21b7e4ba77368eb88b90178aa7455b
Patrícia Junqueira <96187596+PATRICIAJUNQUEIRA@users.noreply.github.com>
2023-07-10 22:01:52 -0300

    DELETE CRUD API

--- Spring-boot-API-MedVoll/src/main/java/med/voll/api/controller/MedicoController.java
+++ Spring-boot-API-MedVoll/src/main/java/med/voll/api/controller/MedicoController.java
@@ -4,7 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -42,5 +44,11 @@ public class MedicoController {
     	var medico = repository.getReferenceById(dados.id());
     	medico.atualizarInformacoes(dados);
     	
+    }
+    
+    @DeleteMapping("/{id}")
+    @Transactional
+    public void excluir(@PathVariable Long id) {
+    	repository.deleteById(id);
     }
 }
